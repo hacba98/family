12991a9d72c2e5d3f78b350e833bac6254c70b4e
Vu Dao Anh Tuan <vudaoanhtuan@gmail.com>
2019-05-09 21:13:50 +0700

    update home page

--- client/src/home/navbar.js
+++ client/src/home/navbar.js
@@ -12,6 +12,10 @@ import {
   DropdownMenu,
   DropdownItem } from 'reactstrap';
 
+import { Button } from 'reactstrap';
+
+import 'font-awesome/css/font-awesome.min.css';
+
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
@@ -28,39 +32,31 @@ export default class NavBar extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Family</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav mr-auto float-right">
+
+          </ul>
+          
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa fa-fw fa-envelope"></i>Thông báo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa fa-fw fa-user"></i>Cá nhân</a>
+            </li>
+          </ul>
+
+          <button class="btn btn-info" >Đăng tin</button>
+        
+        </div>
+      </nav>
+
     );
   }
 }
--- client/src/home/post.js
+++ client/src/home/post.js
@@ -2,21 +2,35 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 import logo_user from './img/user.png'
+import { SSL_OP_TLS_ROLLBACK_BUG } from 'constants';
 
 class Post extends React.Component {
     render() {
         return (
-            <div style={{marginTop:'5px'}}>
-                <Card className="text-left">
-                    <Row>
-                        <Col sm='2'><CardImg style={{width:'100%'}} src={logo_user} alt="Card image cap" /></Col>
-                        <Col>
-                            <CardTitle left><b>Title</b></CardTitle>
-                            <CardBody left>Content</CardBody>
-                        </Col>
-                    </Row>
-                </Card>
+
+<div style={{marginTop: "5px"}}>
+    <div class="card flex-row flex-wrap">
+        <div class="card-header border-0">
+            <img src={logo_user} style={{width: "100px"}} alt=""></img>
+        </div>
+        <div class="card-block px-2">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Description</p>
+            <div class="card-text"><i class="fa fa-map-marker fa-2x"></i> Location</div>
+            <div class="card-text">
+                <i class="fa fa-star fa-2x" style={{color: rgb(0,255,255);}}></i>
+                <i class="fa fa-star fa-2x"></i>
+                <i class="fa fa-star fa-2x"></i>
+                <i class="fa fa-star fa-2x"></i>
+                <i class="fa fa-star fa-2x"></i>
             </div>
+            
+            {/* <a href="#" ><i class="fa fa-heart fa-2x" aria-hidden="true" style={{color: "pink"}}></i></a> */}
+        </div>
+        <div class="w-100"></div>
+    </div>
+</div>
+
         )
     }
 }
