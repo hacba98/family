2ab1062960dcc3c38b7d707c50a843c1d4f51a88
minhphuoc1998 <phuocqhdt@gmail.com>
2019-05-11 15:13:20 +0700

    chat component

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './home/home.js'
 import Post from './post/post.js'
+import Mess from './mess/mess.js'
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Post/>
+      
+      <Mess/>
     </div>
   );
 }
