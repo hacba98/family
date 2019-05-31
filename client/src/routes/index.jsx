b62e515087d67d0ec03b5a4f03e27f1de85e1a9b
Minh Pham <hacba98@gmail.com>
2019-06-01 02:01:25 +0700

    Update API

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,12 +2,14 @@ import Dashboard from "layouts/Dashboard.jsx";
 import LoginPage from "views/Pages/login.jsx";
 import Register from "views/Pages/register.jsx";
 import Feedback from "../feedback/FeedbackPage";
+import FeedbackForm from "../feedback/FeedbackForm";
 
 var indexRoutes = [
   { path: "/login", name: "Login", component: LoginPage },
   { path: "/register", name: "Register", component: Register },
   { path: "/", name: "Home", component: Dashboard, exact: true },
-  { path: "/feedback", name: "Feedback", component: Feedback }
+  { path: "/feedback", name: "Feedback", component: Feedback },
+  { path: "/feedbackform", name: "FeedbackForm", component: FeedbackForm }
 ];
 
 export default indexRoutes;
